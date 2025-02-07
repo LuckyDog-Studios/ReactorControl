2ccc5440605c8c19fe98478af7920b9a970a51a6
noahmjanssen <noahmjanssen@gmail.com>
2025-02-07 13:18:58 +0100

    main menu added

---
+++ scenes/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+func _ready() -> void:
+	$AnimationPlayer.play("RESET")
+
+func _on_start_button_pressed() -> void:
+	$AnimationPlayer.play("start")
+	await $AnimationPlayer.animation_finished
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_settings_button_pressed() -> void:
+	pass # Replace with function body.
--- scripts/dialogue.gd
+++ scripts/dialogue.gd
@@ -1,23 +1,30 @@
-
 extends Control
 
 @onready var text: RichTextLabel = $"MarginContainer/RichTextLabel"
-const letter_delay = 0.1
-
+var letter_delay = 0.05
+@export var text_queue: Array[String] = []
 
 func type_text(line: String) -> void:
+	text.clear()
 	for c in line:
 		text.add_text(c)
 		await get_tree().create_timer(letter_delay).timeout 
+		
+
+func yield_until_space_pressed() -> void:
+	while not Input.is_action_just_pressed("SPACE"):
+		await get_tree().process_frame
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	type_text("Hello there! I'm your robot assistant.");
-	pass # Replace with function body.
-
+	
+	for line in text_queue:
+		await type_text(line)
+		await yield_until_space_pressed()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-	
-		
+	if Input.is_action_pressed("SPACE"):
+		letter_delay = 0.01
+	else:
+		letter_delay = 0.05
